63cc5656008b7cff1de1babb3a1df41c39ed4cbe
Serge <enin.serge@gmail.com>
2016-04-25 15:10:24 +0300

    up to date

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,6 +1,6 @@
 // Check isNaN
 function isNumeric(n){
-	return !isNaN(parseFloat(n)) && isFinite(n); 
+	return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
 
@@ -21,7 +21,7 @@ function fib(n){
  }else{
    return n;
  }
-  
+
 }
 
 console.log(fib(7));
@@ -30,16 +30,16 @@ console.log(fib(7));
 
 function checkSpam(str){
    var toLow = str.toLowerCase();
- 
+
   if(toLow.indexOf('sex') > 0){
     return true;
-    
+
   }else if(toLow.indexOf('spam') > 0){
     return true;
   }else{
     return false;
   }
-  
+
 }
 console.log(checkSpam('get new Sex videos'));// true
 console.log(checkSpam('[SPAM] How to earn fast money?')); // true
@@ -48,20 +48,19 @@ console.log(checkSpam('New PSD template')); // false
 
 // Function slice string more than 20 simbols
 
-function checkSpam(str){ 
-  
+function checkSpam(str){
+
   if(str.length < 20){
     return str;
-    
+
   }else{
     return str.slice(0,20)+'...';
   }
-  
+
 }
 console.log(checkSpam('How to earn fast '));
 console.log(checkSpam('Everyone should learn how to program a computer because it teaches you how to think?'));
-console.log(checkSpam('Everyone should learn how to program a computer because it teaches you how to thinkEveryone should learn how to program a computer because it teaches you how to think?')); 
-console.log(checkSpam('12345678901234567890')); 
+console.log(checkSpam('Everyone should learn how to program a computer because it teaches you how to thinkEveryone should learn how to program a computer because it teaches you how to think?'));
+console.log(checkSpam('12345678901234567890'));
 
-// 
-
+// up to date

244f0fd2e477656e3e52e173918f5e5905a73459
mac <mac@MacBook-Pro-mac.local>
2016-05-16 23:01:20 +0300

    Lesson updated

--- lesson5/script.js
+++ lesson5/script.js
@@ -11,6 +11,8 @@ var salary = 0;
 for(var key in tasksCompleted){
   if(tasksCompleted[key] > salary){
     salary = tasksCompleted[key];
+    var a = key;
+
   }
 }
-console.log(salary);
+console.log(a + ' : ' + salary);

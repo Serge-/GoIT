ac42efd76fbc02ad1e3351b88b16348d4ef6f626
Serge- <enin.serge@gmail.com>
2016-06-07 21:51:31 +0300

    updt

--- lesson5/script.js
+++ lesson5/script.js
@@ -16,3 +16,48 @@ for(var key in tasksCompleted){
   }
 }
 console.log(a + ' : ' + salary);
+
+
+
+// multiplyNumeric function returned numbers multiplyed by 2
+
+var image = {
+    width: 100,
+    height: 400,
+    title: 'Cool image'
+};
+
+function multiplyNumeric(image){
+
+  for(var key in image){
+
+    if(!isNaN(parseFloat(image[key])) && isFinite(image[key])){
+      image[key] = image[key] * 2;
+    }
+
+  }
+   return console.log(image);
+}
+multiplyNumeric(image);
+
+
+// simple calculator. Dialog window required numbers, this numbers will be stacked
+var summ = [];
+
+var count = 0;
+for(var i = 0; ;i++){
+
+  summ[i] = prompt();
+
+  if(!isNaN(parseFloat(summ[i])) && isFinite(summ[i])){
+      count = count + summ[i]++;
+    }
+  else if (summ[i] === ' '){
+    break;
+  }
+   else{
+     break;
+   }
+}
+
+console.log(count);

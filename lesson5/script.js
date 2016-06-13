dabcf0b5436d631b733d76ab2ad8a3800b3f4158
Serge- <enin.serge@gmail.com>
2016-06-14 00:12:48 +0300

    sort

--- lesson5/script.js
+++ lesson5/script.js
@@ -120,4 +120,17 @@ var arr = [1, 2, 3, 4, 5];
 
 arr.sort(function() { return 0.5 - Math.random(); });
 
-console.log( arr ); 
+console.log( arr );
+
+// Sort objects by age
+var vasya = { name: 'Вася', age: 23 };
+var masha = { name: 'Маша', age: 18 };
+var vovochka = { name: 'Вовочка', age: 6 };
+
+var people = [ vasya , masha , vovochka ];
+
+people.sort(function(a,b){return a.age-b.age;});
+
+for(var i=0; i<people.length;i++){
+  console.log(people[i].name);
+}
